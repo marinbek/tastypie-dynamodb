a26900a8412c8ce230f9a9bfc82b4958a6f4ad8a
Marin Bek <marin.bek@gmail.com>
2013-06-26 19:06:44 +0200

    Support for combination hashkeys and relationships

--- tastypie_dynamodb/fields.py
+++ tastypie_dynamodb/fields.py
@@ -1,4 +1,5 @@
-from tastypie.fields import ApiField, ToOneField as TastyOneField
+from tastypie.fields import ApiField, ToOneField as TastyOneField, NOT_PROVIDED
+from django.core.urlresolvers import NoReverseMatch
 
 class PrimaryKeyField(ApiField):
     def hydrate(self, bundle):
@@ -8,11 +9,44 @@ class PrimaryKeyField(ApiField):
         return super(DynamoKeyField, self).hydrate(bundle)
 
 
+"""
+    separator - if your hashkey is something like "SOMETHING:OTHER", ":" is separator and your
+                connected resource has a hash SOMETHING, you can tell this class to separate the
+                value using the separator and use only part of that
+    hashkey_index - after separating key value, which part to use when connecting to a resource
+"""
 class ToOneField(TastyOneField):
+
+    def __init__(self, to, attribute, related_name=None, default=NOT_PROVIDED,
+                 null=False, blank=False, readonly=False, full=False,
+                 unique=False, help_text=None, use_in='all', full_list=True, full_detail=True,
+                 separator=None, hashkey_index=0):
+
+        self.separator = separator
+        self.hashkey_index = hashkey_index
+
+        super(ToOneField, self).__init__(
+            to, attribute, related_name=related_name, default=default,
+            null=null, blank=blank, readonly=readonly, full=full,
+            unique=unique, help_text=help_text, use_in=use_in,
+            full_list=full_list, full_detail=full_detail
+        )
+
 
     def dehydrate(self, bundle):
         resource = self.get_related_resource(bundle.obj)
-        return resource.get_resource_uri(bundle)
+        kwargs = resource.resource_uri_kwargs(bundle)
+
+        url_name = 'api_dispatch_detail'
+
+        if self.separator:
+            val = getattr(bundle.obj, self.attribute).split(self.separator)
+            kwargs['hash_key'] = val[self.hashkey_index]
+            
+        try:
+            return resource._build_reverse_url(url_name, kwargs=kwargs)
+        except NoReverseMatch:
+            return ''
 
 
 class HashKeyField(PrimaryKeyField):
